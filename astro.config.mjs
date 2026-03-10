6183ad42af1f4cfd139a7cbd6cb21eee10492502
i567283 <radoslav.dodnikov@sap.com>
2026-03-10 11:28:58 +0200

    Update domain to whisperstt.com, remove deploy script

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://transcribe.aihelpcenter.dev',
+  site: 'https://whisperstt.com',
   output: 'static',
   integrations: [react(), sitemap()],
   vite: {
